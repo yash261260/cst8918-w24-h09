36b88c6b8ed1fd406e207cd7683bf3eb1738b87f
yash261260 <55890141+yash261260@users.noreply.github.com>
2024-03-12 07:40:08 -0400

    main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,48 @@
+terraform { 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0" 
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "aks" {
-  name     = "my-aks-resource-group"
-  location = "East US"
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "hybrid-h09-aks-rg" 
+  location = "Canada Central" 
 }
 
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "my-aks-cluster"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  dns_prefix          = "myakscluster"
+# Create an AKS Cluster
+resource "azurerm_kubernetes_cluster" "app" {
+  name                = "hybrid-h09-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "hybrid-h09-aks"
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2s"
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_B2s"
+    type                = "VirtualMachineScaleSets"
     enable_auto_scaling = true
-    min_count = 1
-    max_count = 3
+    max_count           = 3
+    min_count           = 1
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    Environment = "Production"
-  }
+  kubernetes_version = "1.29.0"
 }
 
+# Output the kubeconfig
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-
+  value     = azurerm_kubernetes_cluster.app.kube_config_raw
   sensitive = true
 }
